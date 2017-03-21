258e47680bb187239ac5904891eafa1826d121e6
Hilary Lewis <hilarylewis92@gmail.com>
2017-03-21 09:13:36 -0600

    Add app file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from 'react'
 
-class App extends Component {
+export default class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='Home'>
+        <h1 className='light'>
+          Hilary Lewis.
+        </h1>
+        <h1 className='light'>
+          Mathematician. Creative.
+        </h1>
+        <h1 className='light'>
+          Teacher. Learner.
+        </h1>
+        <h1
+          className='bold'>
+          Front End
+          <span className='red'>
+            Developer.
+          </span>
+        </h1>
+        <h1 className='light'>
+          Denver, Colorado.
+        </h1>
       </div>
-    );
+    )
   }
 }
-
-export default App;
